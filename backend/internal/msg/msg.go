56d0409365e4c73063c76190a614ee332c333f35
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-04-06 14:29:32 +0800

    feat: skaffold connection

--- backend/internal/msg/msg.go
+++ backend/internal/msg/msg.go
@@ -1,6 +1,36 @@
 // package msg receives messages from Google Pub/Sub
 package msg
 
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/siuyin/dflt"
+)
+
+var projectID = dflt.EnvString("PROJECT_ID", "someID")
+var ctx = context.Background()
+var client *pubsub.Client
+
+func init() {
+	cl, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("pubsub.NewClient: %v", err)
+	}
+	client = cl
+}
+
+func Close() {
+	if err := client.Close(); err != nil {
+		log.Printf("pubsub.Close: %v", err)
+	}
+}
+
 func Get() string {
 	return "hello"
+}
+
+func SetTopic(name string) error {
+
 }
